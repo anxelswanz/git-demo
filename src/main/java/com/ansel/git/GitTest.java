27a965ac091f0e0a06b538340b277f2af7f04714
ansel <1035205314qq.com>
2023-01-06 21:05:12 +0800

    master commit3

--- src/main/java/com/ansel/git/GitTest.java
+++ src/main/java/com/ansel/git/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("hello gitnababa!");
         System.out.println("hello master!");
         System.out.println("hello hot-fix!");
+        System.out.println("push!");
+        System.out.println("push");
     }
 }
