feff33cf40663197b5e4eeb42d4e86f85dabc232
ansel <1111>
2023-01-06 12:50:37 +0800

    first commit

---
+++ src/main/java/com/ansel/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ansel.git;
+
+/**
+ * @author Ansel Zhong
+ * coding time
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
